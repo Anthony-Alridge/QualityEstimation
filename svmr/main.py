466e2c17498f2ba73b88043ec64f30446290e382
Anthony Alridge <anthonyalridge270@gmail.com>
2020-02-25 15:03:23 +0000

    changes

--- svmr/main.py
+++ svmr/main.py
@@ -3,6 +3,8 @@ import argparse
 import numpy as np
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 
 nlp_en = spacy.load('en_core_web_sm')
 nlp_de = spacy.load('de_core_news_sm')
@@ -11,7 +13,10 @@ lang_to_model = {'en': nlp_en, 'de': nlp_de}
 
 # Counts the number of named entities in the sentence
 def ner(text):
-    pass
+    nlp = lang_to_model[lang]
+    tags_to_idx = corpus_info['tags']
+    doc = nlp(text)
+    return np.array([len(doc.ents)])
 
 # Returns array with frequency of each part of speech tag.
 def pos(text, lang, corpus_info):
@@ -25,21 +30,19 @@ def pos(text, lang, corpus_info):
             features[tags_to_idx[tag]] += 1
     return features
 
-# The depth and width of the consituency tree.
+# The depth, width, number of nodes in the consituency tree.
 def tree():
     pass
 
 # The length of the sentence.
-def length():
-    pass
-
-# The probabality score assigned to the sentece by a language model.
-def fluency():
-    pass
+def length(text, lang):
+    nlp = lang_to_model[lang]
+    tokens = nlp(text)
+    return np.array([len(tokens)])
 
 # Apply all features and concatenate arrays together into a single row.
 def get_features_for_text(text, lang, corpus_info):
-    return pos(text, lang, corpus_info)
+    return np.append(pos(text, lang, corpus_info), length(text, lang))
 
 # Apply features to each line.
 # Each row in return is feautures for a single line
@@ -52,8 +55,7 @@ def get_features(src, target, lang, corpus_info):
     return np.vstack(tuple(data))
 
 # TODO: make work for chinese
-def load_language(language):
-    type = 'train'
+def load_language(language, type = 'train'):
     f = open('../data/en-de/' + type + '.ende.src', encoding='utf-8') # Open file on read mode
     lines_en = f.read().split("\n") # Create a list containing all lines
     f.close() # Close file
@@ -70,7 +72,7 @@ def load_language(language):
     scores = [float(score) for score in scores if score != '']
     return lines_en, lines_de, scores
 
-def get_corpus_info(src, target):
+def get_tags(src, target):
     tags = set()
     src_corpus = nlp_en('\n'.join(src))
     target_corpus = nlp_de('\n'.join(target))
@@ -78,35 +80,33 @@ def get_corpus_info(src, target):
         tags.add(token.pos_)
     for token in target_corpus:
         tags.add(token.pos_)
+    return {tag: i for i, tag in enumerate(tags) }
+
+def get_corpus_info(src, target, scores):
     corpus_info = {}
-    corpus_info['tags'] = {tag: i for i, tag in enumerate(tags) }
+    corpus_info['tags'] = get_tags(src, target)
     return corpus_info
 
 def evaluate(preds, y):
-    sum_x = preds.sum()
-    sum_y = y.sum()
-    sum_xsq = np.dot(preds, preds)
-    sum_ysq = np.dot(y, y)
-    sum_xy = np.dot(preds, y)
-    error = np.sum(np.abs(preds - y))
-    error_sq = np.dot(preds - y, preds - y)
-    num_samples = preds.shape[0]
-    r = num_samples * sum_xy + sum_x * sum_y * np.reciprocal(np.sqrt(num_samples * sum_xsq - sum_x ** 2)) * np.reciprocal(np.sqrt(num_samples * sum_ysq - sum_y ** 2))
-    mae = error / num_samples
-    rmse = np.sqrt(error_sq / num_samples)
+    r = np.corrcoef(preds, y)[0, 1]
+    mae = mean_absolute_error(y, preds)
+    rmse = np.sqrt(mean_squared_error(y, preds))
     return (r, mae, rmse)
 
 def main(target_lang, data_dir):
     # Prepare data
-    src, target, scores = load_language(target_lang)
-    scores = np.array(scores)
-    corpus_info = get_corpus_info(src, target)
+    src, target, scores_ = load_language(target_lang)
+    dev, target_dev, scores_dev_ = load_language(target_lang, 'dev')
+    scores = np.array(scores_)
+    scores_dev = np.array(scores_dev_)
+    corpus_info = get_corpus_info(src, target, scores_)
     # Apply features
     train = get_features(src, target, target_lang, corpus_info)
+    test = get_features(dev, target_dev, target_lang, corpus_info)
     # test, test_labels = get_features(test_data)
     print(
         f'Training shape is {train.shape} and labels is {scores.shape}')
-    # print(f'Testing shape is {test.shape} and labels is {test_labels.shape}')
+    print(f'Testing shape is {test.shape} and labels is {scores_dev.shape}')
     # # Train classifier
     svc = SVR()
     Cs = [2**k for k in range(-2, 2)]
@@ -114,12 +114,12 @@ def main(target_lang, data_dir):
     clf = GridSearchCV(svc, params)
     model = clf.fit(train, scores)
     # # Evaluate model.
-    # test_accuracy = model.score(test, test_labels)
     train_r, train_mse, train_rmse = evaluate(model.predict(train), scores)
+    test_r, test_mse, test_rmse = evaluate(model.predict(test), scores_dev)
     print(f'Parameters used are {model.best_params_}')
     print('Scores:')
-    #print(f'Accuracy on test set:  {test_accuracy}')
     print(f'R on train set: {train_r}, mse: {train_mse}, rmse: {train_rmse}')
+    print(f'R on dev set: {test_r}, mse: {test_mse}, rmse: {test_rmse}')
 
 
 if __name__ == '__main__':
